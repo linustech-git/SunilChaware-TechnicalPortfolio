54a791a770160cc0fb11d9051f06d9eff1725f78
Sunil Chaware <chawaresunil1987@gmail.com>
2026-04-22 19:59:06 +0530

    Made css changes

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const archive = defineCollection({
-  type: "content",
+  loader: glob({ pattern: "**/*.md", base: "./src/content/archive" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
